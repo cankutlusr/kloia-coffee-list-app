6a208de5f1f21ef4c6f960aa7e22c8eee63510fe
cankutlusr <cankutlu17@outlook.com>
2021-09-18 15:16:01 +0300

    kloia: not found style added

--- src/app/page/coffee-list/Coffees.js
+++ src/app/page/coffee-list/Coffees.js
@@ -3,6 +3,7 @@ import PageLayout from "../../shared/page-layout/index";
 import Sidebar from "../../components/sidebar"
 import Content from "../../components/content"
 import {useCoffees} from '../../context/coffees' 
+import CoffeeLogo from '../../../assets/images/coffee.png'
 
 const Coffees = (props) => {
     const {coffees,filteredCoffees,setFilteredCoffees,filteredSearchCoffees,setFilteredSearchCoffees,setActiveCategoryName} = useCoffees();
@@ -34,13 +35,29 @@ const Coffees = (props) => {
         }
     },[props.match.params.category])
 
+    const container = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      };
+
+    const image = {
+        height:300,
+        width: 300,
+    };
+
+    const h1 = {
+        marginLeft: 20,
+        fontSize: 200,
+        fontFamily: 'Exo'
+    }
     return (
         <PageLayout 
             sidebar={<Sidebar/>} 
             content={
                 <>
                 {filteredCoffees?.map((coffee,index) => <Content key={index} header={coffee.title} description={coffee.description} ingredients={`Ingredients: ${coffee.ingredients.join(", ")}`}  /> )}
-                {filteredCoffees.length === 0 && <div>No Coffee Found :`(`</div>}
+                {filteredCoffees.length === 0 && <div style={container}><h1 style={h1}>No Coffee Found!</h1><img style={image} src={CoffeeLogo} /></div>}
                 </>
             }
         />
